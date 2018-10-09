5a0d8647058781aad7d4a0762b52da04c479ae8e
SamJay0 <samjay0.254@gmail.com>
2018-10-09 22:20:54 +0300

    got started

---
+++ Inventory/src/inventory/Inventory.java
@@ -0,0 +1,12 @@
+package inventory;
+/**
+ * @author SamJay
+ */
+public class Inventory {
+
+    public static void main(String[] args) {
+        System.out.println("just got started");
+        
+    }
+
+}
