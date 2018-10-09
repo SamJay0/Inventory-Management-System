234c15a79d761d4c9f11b26f9f07482f014605e0
SamJay0 <samjay0.254@gmail.com>
2018-10-09 23:26:12 +0300

    Added the choice function

--- Inventory/src/inventory/Inventory.java
+++ Inventory/src/inventory/Inventory.java
@@ -3,9 +3,22 @@ package inventory;
  * @author SamJay
  */
 public class Inventory {
+    
+    //user choice
+    public static void enterChoice(){
+        System.out.println("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n"
+                + "\tWelcome to the Simple Inventory Management System\n"
+                + "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n"
+                + "Please Select from the following options\n"
+                + "Press 1 to Register a product for sale\n"
+                + "Press 2 to Buy a product to the Cart\n"
+                + "Press 4 to view all Available Products\n"
+                + "Press 5 to check out\n"
+                + "Press 7 to exit");
+    }
 
     public static void main(String[] args) {
-        System.out.println("just got started");
+        enterChoice();
         
     }
 
